403a6660d4a82bc5f44a18b7439230e5c4f1fb3e
Art Yudin <artyudin@outlook.com>
2023-03-07 15:40:07 -0500

    react

---
+++ static/myreactfiles/like_button.js
@@ -0,0 +1,33 @@
+"use strict";
+<h2 class="title">Empowering businesses with SaaS technology</h2>
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false, years: [] };
+  }
+
+//   componentDidMount() {
+//     axios.get(`http://128.199.5.86/api/market/ford/mustang/`).then((res) => {
+//       console.log(res);
+//       const years = res.data;
+//       this.setState({ years });
+//     });
+//   }
+
+  render() {
+    if (this.state.liked) {
+      return (<div><h2 class="title">Huy sobachi</h2></div>);
+    }
+
+    return (
+        <div><h2 class="title" onClick={() => this.setState({ liked: true })}>Empowering businesses with SaaS technology</h2></div>
+    );
+  }
+}
+
+
+const domContainer = document.querySelector("#like_button_container");
+ReactDOM.render(e(LikeButton), domContainer);
