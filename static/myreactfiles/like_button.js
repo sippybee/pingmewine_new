f05863cc981597a4acc4fd06af6786a6879c7462
Art Yudin <artyudin@outlook.com>
2023-03-16 23:24:28 -0400

    react

--- static/myreactfiles/like_button.js
+++ static/myreactfiles/like_button.js
@@ -98,7 +98,7 @@ class LikeButton extends React.Component {
               className="video-play-btn-hover"
               href="https://www.youtube.com/embed/Wimkqo8gDZ0"
              > 
-              <FontAwesomeIcon icon="fa-regular fa-circle-play" />
+              <i className="fa-regular fa-circle-play"></i>
               <h6 className="d-inline-block">how we work</h6>
             </a>
           </div>
